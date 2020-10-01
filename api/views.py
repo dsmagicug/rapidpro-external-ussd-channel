e098215b6fa23676e5daff6a3eca7852fbf09b6a
Keeya Emmanuel <keeyaemmanuel12@gmail.com>
2020-10-02 01:34:33 +0300

    intergrating rapidPro

--- api/views.py
+++ api/views.py
@@ -6,11 +6,14 @@ import requests
 from ast import literal_eval
 import secrets
 from websocket import create_connection
-from handlers.utils import ProcessAggregatorRequest, RP_RESPONSE_FORMAT, RP_RESPONSE_STATUSES
+from handlers.utils import ProcessAggregatorRequest, RP_RESPONSE_FORMAT, RP_RESPONSE_STATUSES, standard_urn, \
+    SESSION_STATUSES
 from channel.models import USSDChannel, USSDSession
 from core.utils import access_logger, error_logger
 from django.http import QueryDict
+from rest_framework.parsers import JSONParser
 from ast import literal_eval
+import re
 
 # Create your views here.
 
@@ -38,15 +41,16 @@ def push_ussd(payload):
 
 @api_view(['POST'])
 def send_url(request):
-    # access_logger.info(request.META)
+    # access_logger.info(str(request.META))
     try:
-        if isinstance(request.data, QueryDict):  # for application/x-www-form-urlencoded
+        if request.META["CONTENT_TYPE"] == "application/x-www-form-urlencoded":
             data = request.data
             content = data.dict()
         else:
-            content = request.data  # for application/json
+            # TODO handle other content types
+            content = set()
         # decrement key1
-        to = content['to']
+        to = content['to_no_plus']
         key1 = f"MO_MT_KEY_{to}"
         key2 = f"MSG_KEY_{to}"
         key1_val = int(r.decr(key1))
@@ -54,7 +58,6 @@ def send_url(request):
             r.lpush(key2, str(content))
         else:
             msg_extras = dict(msg_type="Outgoing", status="Received")
-            content = request.data
             content.update(msg_extras)
             push = push_ussd(content)
             # reset key to 1
@@ -75,25 +78,32 @@ def call_back(request):
     # access_logger.info(str(request.META))
     try:
         request_dict = literal_eval(json.dumps(request.data['info']))  # from GCE
-        # request_dict = literal_eval(json.dumps(request.data))
+        #request_dict = literal_eval(json.dumps(request.data))
         sr = ProcessAggregatorRequest(request_dict)
         standard_request_string = sr.process_handler()
-        current_session_id = sr.log_session()
-
+        current_session = sr.log_session()
+        is_new_session = sr.is_new_session()
+        still_in_flow = sr.is_in_flow()
+        print(is_new_session, still_in_flow)
         handler = sr.get_handler()
         end_action = handler.signal_end_string
         reply_action = handler.signal_reply_string
-
         urn = standard_request_string['from']
+        if is_new_session:
+            text = " "
+        else:
+            text = standard_request_string["text"]
+        allowed_urn = standard_urn(urn)
         # TODO tell if its first string
+
         rapid_pro_request = {
-            "from": urn,
-            "text": standard_request_string["text"]
+            "from": allowed_urn,
+            "text": text
         }
         # create redis keys
-        key1 = f"MO_MT_KEY_{urn}"
+        key1 = f"MO_MT_KEY_{allowed_urn}"
         r.set(key1, 1)  # proven necessary or else
-        key2 = f"MSG_KEY_{urn}"
+        key2 = f"MSG_KEY_{allowed_urn}"
         """""Channel details """""
         # first fetch all since you are not sure of the id (in cases
         # where once channel was deleted, (impossible to do that within the app though)
@@ -105,7 +115,6 @@ def call_back(request):
         # receive_url is used to send msgs to rapidpro
         receive_url = channel.rapidpro_receive_url
         # req = requests.post(receive_url, json.dumps(rapid_pro_request), headers=HEADERS)
-
         req = requests.post(receive_url, rapid_pro_request, headers=HEADERS)
         if req.status_code == 200:
             # increment key1
@@ -119,24 +128,66 @@ def call_back(request):
                 if status == RP_RESPONSE_STATUSES['waiting']:
                     action = reply_action
                 else:
-                    # mark session complete
-                    USSDSession.objects.filter(session_id=current_session_id).update(status="Completed",
-                                                                                     badge="success")
+                    # mark session complete and give is a green badge
+                    current_session.status = SESSION_STATUSES['COMPLETED']
+                    current_session.badge = "success"
+                    current_session.save()
                     action = end_action
                 new_format = dict(text=text, action=action)
                 response = sr.get_expected_response(new_format)
             else:
-                # pop key2 to avoid sync issues
-                USSDSession.objects.filter(session_id=current_session_id).update(status="Timed Out", badge="danger")
+                # mark session timed out and give it a red badge
+                current_session.status = SESSION_STATUSES['TIMED_OUT']
+                current_session.badge = "danger"
+                current_session.save()
+
                 error_logger.debug(f"Response timed out for redis key {key2}")
-                r.lpop(key2)
+                # pop key2
+                # r.lpop(key2)
                 res_format = dict(text="Response timed out", action=end_action)
                 response = sr.get_expected_response(res_format)
         else:
             res_format = dict(text="External Application unreachable", action=end_action)
+            error_logger.exception(req.content)
             response = sr.get_expected_response(res_format)
         return Response(response, status=200)
     except Exception as err:
         error_logger.exception(err)
         response = {"responseString": "External Application unreachable", "action": "end"}
         return Response(response, status=500)
+
+
+# the endpoints below are for simulating rapidpro
+@api_view(['POST', 'GET', 'PUT'])
+def receive(request):
+    # access_logger.info(str(request.META))
+    data = request.data
+
+    clean_data = data.dict()
+    try:
+        url = "http://localhost:5000/adaptor/processor"
+        req = requests.post(url, clean_data, headers=HEADERS)
+        status = req.status_code
+        if status == 200:
+            return Response(dict(message="Receive got it"), status=200)
+        else:
+            return Response(dict(message="Some error"), status=200)
+    except Exception as err:
+        error_logger.exception(err)
+        print(err)
+
+
+@api_view(['POST'])
+def processor(request):
+    data = request.data
+    phrases = [
+        "What month did you start your last period?\n1.Jan\n2.Feb\n"
+    ]
+    text = secrets.choice(phrases)
+    _from = data['from']
+    response = {"text": text, "to": _from, "status": "W"}
+    # call send url
+    url = "http://localhost:5000/adaptor/send-url"
+    req = requests.post(url, response, headers=HEADERS)
+    if req.status_code == 200:
+        return Response(response, status=200)
